8c38beeead837fb1b292e0d1cedad2987caa7c73
Skoshi <CiaranMaudsley@protonmail.com>
2026-02-12 01:51:47 +0000

    Did I rewrite everything? yes, should I have commited in in smaller parts? probably

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,126 @@
 
-
 const MAD_LIBS_DIR: &'static str = "mad_libs";
+
+
+struct MadLib {
+    mad_lib :String,
+    fill_in_words: Vec<String>,
+    filled_in_words: Vec<String>
+}
+
+enum MadLibsParseError {
+    NoFillInWords,
+    MissingClosingBrackets
+}
+
+impl MadLib {
+    fn parse_mad_lib(&mut self) -> Result<(), MadLibsParseError>{
+        let mut is_in_brackets: bool = false;
+        let mut current_word: String = String::new();
+
+        for ch in self.mad_lib.chars(){
+            match ch {
+                '[' => {
+                    is_in_brackets = true;
+                    continue;
+                },
+                ']' => {
+                    is_in_brackets = false;
+                    self.fill_in_words.push(current_word.clone());
+                    current_word.clear();
+                    continue;
+                },
+
+                _ => {}
+            };
+
+            if is_in_brackets {
+                current_word.push(ch);
+            }
+
+        }
+        
+        if is_in_brackets {
+            return Err(MadLibsParseError::MissingClosingBrackets)
+        }
+        else if self.fill_in_words.len() <= 0 {
+            return Err(MadLibsParseError::NoFillInWords);
+        };
+        
+        Ok(())
+
+    }
+
+    fn fill_in_words(&mut self) -> Result<(), std::io::Error> {
+        
+        let mut current_word: String = String::new();
+
+        let input = std::io::stdin();
+
+        for fill_in_word in self.fill_in_words.iter() {
+
+            println!("Please enter a {fill_in_word}");
+            match input.read_line(&mut current_word) {
+                Ok(_) => (),
+
+                Err(e) => {
+                    return Err(e);
+                }
+            }
+            
+            // This is to remove the \n char at the end of the string
+            current_word.pop();
+
+            self.filled_in_words.push(current_word.clone());
+            current_word.clear();
+        };
+        return Ok(())
+
+    }
+
+    fn display_mad_lib(&mut self) {
+        let mut iter_count: usize = 0;
+        
+        for word in self.filled_in_words.iter_mut() {
+            if word == "" {
+                *word = String::from("Word Not Entered");
+            };
+
+            let mut pattern :String = String::new();
+            
+            pattern.push('[');
+            pattern.push_str(&self.fill_in_words[iter_count]);
+            pattern.push(']');
+
+            self.mad_lib = self.mad_lib.replacen(&pattern, word, 1);
+
+            pattern.clear();
+
+            println!("Fill in word is: {}", &self.fill_in_words[iter_count]);
+
+            iter_count += 1;
+
+        }
+
+        println!("{}", self.mad_lib);
+    }
+}
 
 fn main() {
     println!("Welcome to Skoshi's Mad Lib Terminal game!");
 
     let mad_libs_dir = std::path::Path::new(MAD_LIBS_DIR);
 
-    if !mad_libs_dir.exists() {
-        println!("No {MAD_LIBS_DIR} directory found...");
-        println!("Making {MAD_LIBS_DIR} directory...");
+    match check_mad_lib_dir(&mad_libs_dir) {
+        Ok(_) => (),
 
-        match std::fs::create_dir(mad_libs_dir){
-            Ok(_) => println!("Successful made {MAD_LIBS_DIR}"),
-
-            Err(e) => println!("Failed to create {MAD_LIBS_DIR}: {e}")
+        Err(e) => {
+            println!("{e}");
+            return;
         }
     }
 
-    if !mad_libs_dir.is_dir(){
-        println!("{MAD_LIBS_DIR} found, but it appears not to be a directory, 
-            please delete or rename it so the program can make the correct {MAD_LIBS_DIR}")
-    }
-
+    //Getting an iterator of all the files in mad_libs/
     let mad_libs_iter: std::fs::ReadDir;
 
     match std::fs::read_dir(mad_libs_dir){
@@ -34,13 +132,11 @@ fn main() {
         }
     }
 
-    let mut x :i32 = 0;
+    //Iterating over the iterator and saving each directory entry into a Vec
     let mut mad_libs :Vec<std::fs::DirEntry> = vec![];
     for entry in mad_libs_iter {
-        x += 1;
         match entry {
             Ok(entry) => {
-                println!("{x}. {:?}", entry.file_name());
                 mad_libs.push(entry);
             }
             Err(e) => {
@@ -50,8 +146,41 @@ fn main() {
         }
     }
 
+    play_mad_lib(&mad_libs);
+
+    let mut user_input: String = std::string::String::new();
+    loop {
+        println!("Would you like to play again? [Y/N]");
+
+        std::io::stdin()
+                .read_line(&mut user_input)
+                .expect("Failed to read line");
+        
+        match user_input.trim().to_lowercase().as_str() {
+            
+            "y" => play_mad_lib(&mad_libs),
+
+            "n" => break,
+
+            _ => {
+                println!("Unknown input")
+            }
+        }
+    }
+
+    println!("Thank you for playing my Mad Lib game!")
+
+}
+
+fn select_mad_lib(mad_libs_files: &Vec<std::fs::DirEntry>) -> usize {
     let mut user_input_str: String = String::new();
-    let mut user_input_usize: usize;
+    let user_input_usize: usize;
+
+    //Prints out the number and name of each file for the user to select
+    for (x, file) in mad_libs_files.iter().enumerate() {
+        println!("{}. {:?}", x+1, file.file_name())
+    }
+    
     loop {
         println!("Please enter the number of the Mad Lib you want to play");
         std::io::stdin()
@@ -65,33 +194,69 @@ fn main() {
             */
             match user_input_str.trim().parse::<usize>() {
                 Ok(num) => {
-                    if num <= mad_libs.len() && num > 0 {
+                    if num <= mad_libs_files.len() && num > 0 {
                         user_input_usize = num - 1;
                         break;
                     }
                     else {
                         println!("Number is either too long or too big! Please make sure to enter 
-                        a number greater than 0 but less than {}", mad_libs.len());
+                        a number greater than 0 but less than {}", mad_libs_files.len());
                         continue;
                     }
                 }
                 Err(_) => continue,
             };
+        };
+        return user_input_usize;
+}
+
+fn check_mad_lib_dir(mad_libs_dir: &std::path::Path) -> Result<(), std::io::Error>{
+    if !mad_libs_dir.exists() {
+            println!("No {MAD_LIBS_DIR} directory found...");
+            println!("Making {MAD_LIBS_DIR} directory...");
+
+            match std::fs::create_dir(mad_libs_dir){
+                Ok(_) => println!("Successful made {MAD_LIBS_DIR}"),
+
+                Err(e) => {
+                    println!("Failed to create {MAD_LIBS_DIR}: {e}");
+                    return Err(e);
+                }
+            }
         }
 
+        if !mad_libs_dir.is_dir(){
+            println!("{MAD_LIBS_DIR} found, but it appears not to be a directory, 
+                please delete or rename it so the program can make the correct {MAD_LIBS_DIR}");
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::AlreadyExists,
+                "Path exists but is not a directory"));
+        }
 
-    let mut mad_lib: String = std::string::String::new();
+        Ok(())
+}
+
+fn play_mad_lib(mad_libs: &Vec<std::fs::DirEntry>) {
+    
+    let user_input_usize: usize = select_mad_lib(&mad_libs);
+
+    let mut mad_lib: MadLib = MadLib { 
+        mad_lib: String::new(), 
+        fill_in_words: vec![], 
+        filled_in_words: vec![]
+    };
 
     match std::fs::read_to_string(mad_libs[user_input_usize].path()){
-        Ok(s) => mad_lib = s,
+        Ok(s) => mad_lib.mad_lib = s,
 
-        Err(_) => {}
+        Err(_) => {
+            println!("Mad Lib File could not be read!");
+            return
+        }
     }
     
-    let mut fill_in_words: Vec<String> = vec![];
-
-    match parse_mad_lib_file(mad_lib.clone()) {
-        Ok(fill_in) => fill_in_words = fill_in,
+    match mad_lib.parse_mad_lib() {
+        Ok(_) => (),
 
         Err(e) => {
             match e {
@@ -107,11 +272,9 @@ fn main() {
         }
     }
 
-    let mut filled_in_words :Vec<String> = vec![];
+    match mad_lib.fill_in_words() {
 
-    match get_fill_in_words(fill_in_words.clone()) {
-
-        Ok(ans) => filled_in_words = ans,
+        Ok(_) => (),
 
         Err(e) => {
             println!("Error getting fill in words: {e}");
@@ -120,115 +283,7 @@ fn main() {
 
     }
 
-    println!("Filled in words are: {:?}", filled_in_words.clone());
+    println!("Filled in words are: {:?}", mad_lib.filled_in_words.clone());
 
-    display_mad_lib(mad_lib, fill_in_words, &mut filled_in_words);
-
-
-
-}
-
-enum MadLibsParseError {
-    NoFillInWords,
-    MissingClosingBrackets
-}
-
-
-fn parse_mad_lib_file(raw_mad_lib: String) -> Result<Vec<String>, MadLibsParseError> {
-    let mut is_in_brackets: bool = false;
-    let mut current_word: String = String::new();
-    let mut fill_in_words: Vec<String> = vec![];
-
-    for ch in raw_mad_lib.chars(){
-        match ch {
-            '[' => {
-                is_in_brackets = true;
-                continue;
-            },
-            ']' => {
-                is_in_brackets = false;
-                fill_in_words.push(current_word.clone());
-                current_word.clear();
-                continue;
-            },
-
-            _ => {}
-        };
-
-        if is_in_brackets {
-            current_word.push(ch);
-        }
-
-    }
-
-    
-    if is_in_brackets {
-        return Err(MadLibsParseError::MissingClosingBrackets)
-    }
-    else if fill_in_words.len() <= 0 {
-        return Err(MadLibsParseError::NoFillInWords);
-    }
-    else {
-        return Ok(fill_in_words);
-    }
-}
-
-fn get_fill_in_words(fill_in_words: Vec<String>) -> Result<Vec<String>, std::io::Error> {
-
-    let mut filled_in_words: Vec<String> = vec![];
-
-    let mut current_word: String = String::new();
-
-    let input = std::io::stdin();
-
-    for fill_in_word in fill_in_words.iter() {
-
-        println!("Please enter a {fill_in_word}");
-        match input.read_line(&mut current_word) {
-            Ok(_) => (),
-
-            Err(e) => {
-                return Err(e);
-            }
-        }
-        
-        // This is to remove the \n char at the end of the string
-        current_word.pop();
-
-        filled_in_words.push(current_word.clone());
-        current_word.clear();
-
-    };
-
-    return Ok(filled_in_words);
-
-    
-}
-
-fn display_mad_lib(mut raw_mad_lib: String, fill_in_words: Vec<String>, filled_in_words: &mut Vec<String>) {
-    
-    let mut iter_count: usize = 0;
-    
-    for word in filled_in_words.iter_mut() {
-        if word == "" {
-            *word = String::from("Word Not Entered");
-        };
-
-        let mut pattern :String = String::new();
-        
-        pattern.push('[');
-        pattern.push_str(&fill_in_words[iter_count]);
-        pattern.push(']');
-
-        raw_mad_lib = raw_mad_lib.replacen(&pattern, word, 2);
-
-        pattern.clear();
-
-        println!("Fill in word is: {}", &fill_in_words[iter_count]);
-
-        iter_count += 1;
-
-    }
-
-    println!("{}", raw_mad_lib);
+    mad_lib.display_mad_lib();
 }
